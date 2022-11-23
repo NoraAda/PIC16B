cfce9b327441687bb57eaa8c7b39defbec4f3e07
Nora Adadurova <ada.nora2002@gmail.com>
2022-11-23 13:15:49 -0800

    added documintation and comments to the code

--- Final Project/selenium_based_manga_monitoring.py
+++ Final Project/selenium_based_manga_monitoring.py
@@ -45,6 +45,21 @@ def extract_integer_from_string(string):
 @print_decorator
 def get_user_input():
     
+    '''
+    A simple functioun to ineractively get 2 input values (and globalize them as variables) from User: 
+    
+    user_input_1 - a title of Manga; 
+    user_input_2 - a number of chapters that user have already read; 
+    
+    and user_input_1_adapted handled from user_input_1 and processed with regex.
+    
+    Note: in fact, there is also an input between user_input_1 and user_input_1 actually for User 
+          with question 'Did you read it? (Y|N)' 
+          (so that 'n' answer means that user_input_2 == '0' but anything else follows by question 
+           'What chapter you stop on? (one number)' to get another user_input_2).
+    
+    '''
+    
     global user_input_1, user_input_2, user_input_1_adapted
     
     user_input_1 = input('What Manga you want to check on new chapters? (enter full title correctly) ')
@@ -73,6 +88,14 @@ def get_user_input():
 def start_or_reload_display(driver_name='Chrome', 
                             detach_display=True):
     
+    '''
+    Returns 2 objects: selenium.webdriver and pyvirtualdisplay.Display (None if detach_display argument is False)
+    Supported values for driver_name argument are 'Chrome' and 'Safari'
+    
+    Note: XVFB should be installed in OS if detach_display argument is True.
+    
+    '''
+    
     if detach_display:
         
         virtual_display = Display(
@@ -98,6 +121,28 @@ def get_latest_chapter_text(selenium_object,
                             good_css_selector_num=0, 
                             bad_css_selector='.not-found-content'):
     
+    '''
+    Extracts and returns a string value with useful text from specified good_css_selector.
+    
+    Expected text with a pointer to a number of the latest chapter in good_css_selector 
+    (when prepared webdriver at a page with a content about some kind of Manga).
+    
+    selenium_object - prepared webdriver; 
+    good_css_selector - CSS-selector where is the text about the latest chapter for the Manga presented; 
+    url_to_print - specified url (of resource's mainpage, for example, as well as the direct page of the Manga); 
+    
+    good_css_selector_num - if there are more than one text-blocks are presented when handle 
+                            with good_css_selector argument (0 is the first one from list); 
+    bad_css_selector - CSS-selector where is the text represents a page with 404 (not found) error 
+                       to handle it instead of getting message about fail.
+    
+    If good_css_selector element is not found on the page, there is one of the messages 
+    (about not found Manga on this resource if bad_css_selector argument is specified 
+     or, in another way, about fail while checking this resource) is being printed 
+    with specified url_to_print argument.
+    
+    '''
+    
     try:
         latest_chapter_text = selenium_object.find_elements("css selector", 
                                                             good_css_selector
@@ -116,6 +161,17 @@ def get_latest_chapter_text(selenium_object,
 def get_new_chapters_integer(latest_chapter_text, 
                              chapters_already_read):
     
+    '''
+    Extracts an integer number meaning the latest chapter of the Manga 
+    from any text in latest_chapter_text argument pointing to it 
+    and returns an integer of new chapters count (calculated by chapters_already_read argument) 
+    that user can read.
+    
+    latest_chapter_text - a string with useful information about the latest chapter of the Manga; 
+    chapters_already_read - a number of chapters that user have already read.
+    
+    '''
+    
     chapters_already_read = extract_integer_from_string(chapters_already_read)
     
     try:
@@ -128,6 +184,14 @@ def get_new_chapters_integer(latest_chapter_text,
 
 def print_output_message(url_to_print, 
                          new_chapters_integer):
+    
+    '''
+    Constructs and prints a message depending on calculated count of new chapters of the Manga to output.
+    
+    url_to_print - specified url (of resource's mainpage, for example, as well as the direct page of the Manga); 
+    new_chapters_integer - the calculated count of new chapters that user can read.
+    
+    '''
     
     message = f'According to {url_to_print} '
     
@@ -151,6 +215,17 @@ def get_processed_core_page(selenium_object,
                             good_css_selector_num=0, 
                             bad_css_selector='.not-found-content'):
     
+    '''
+    Wraps up sequential calls to 
+    
+    get_latest_chapter_text, 
+    get_new_chapters_integer and 
+    print_output_message 
+    
+    functions defined above. 
+    
+    '''
+    
     latest_chapter_text = get_latest_chapter_text(selenium_object=selenium_object, 
                                                   good_css_selector=good_css_selector, 
                                                   url_to_print=url_to_print, 
@@ -170,8 +245,10 @@ def get_processed_core_page(selenium_object,
 
 get_user_input()
 
+# prepare webdriver
 selenium_spider, virtual_display = start_or_reload_display(driver_name='Chrome', detach_display=True)
 
+# specify a group of resources with the same CSS elements
 urls_batch_1 = ['https://1stkissmanga.io', 
                 'https://zinmanga.com', 
                 'https://coffeemanga.com', 
@@ -179,14 +256,20 @@ urls_batch_1 = ['https://1stkissmanga.io',
 
 for url in urls_batch_1:
     
+    # construct the direct link to manga to check it if it exists
     direct_link_to_manga = f'{url}/manga/{user_input_1_adapted}'
+    # connect webdriver to the direct link
     selenium_spider.get(direct_link_to_manga)
     
+    # get the list of elements from '.heading' CSS-selectors
     list_of_heading_elements_found = selenium_spider.find_elements("css selector", '.heading')
     
+    # check if the first '.heading' CSS-selector found text is about specific 404 error
     if len(list_of_heading_elements_found) > 0 and (
        list_of_heading_elements_found[0].text != 'Oops! page not found.'):
+        # if not - process this page
         
+        # check the page on new chapters of the manga
         get_processed_core_page(selenium_object=selenium_spider, 
                                 good_css_selector='li.wp-manga-chapter:nth-child(1) > a:nth-child(1)', 
                                 url_to_print=url, 
@@ -196,11 +279,13 @@ for url in urls_batch_1:
         
     else:
         
+        # construct the link with suitable mangas found on searching page of the resource
         user_input_1_readapted = '+'.join(user_input_1.lower().split())
         searching_url = url + f'/?s={user_input_1_readapted}&post_type=wp-manga'
-        
+        # and connect webdriver to this link
         selenium_spider.get(searching_url)
         
+        # check the page on new chapters of the manga
         get_processed_core_page(selenium_object=selenium_spider, 
                                 good_css_selector='span.font-meta:nth-child(2)', 
                                 url_to_print=url, 
